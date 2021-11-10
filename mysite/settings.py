7f5010c250c2abd06ff2e1e49dd1c655f78a9e7b
Анна <a.marshalova@g.nsu.ru>
2021-11-10 22:08:36 +0700

    03.11: settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(gkml$+up0hd=ht@2z#_p)%5qs27p1s64&2pkicb-&(4(z=e+j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
